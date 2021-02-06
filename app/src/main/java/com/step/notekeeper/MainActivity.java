4405b5df1db4d13d0de642b6d5221c6a60162448
mustafayevt <mustafayevtural46@gmail.com>
2021-02-06 15:18:47 +0400

    Add note button made functional.

--- app/src/main/java/com/step/notekeeper/MainActivity.java
+++ app/src/main/java/com/step/notekeeper/MainActivity.java
@@ -2,13 +2,26 @@ package com.step.notekeeper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final int REQUEST_CODE_ADD_NOTE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        ImageView imageAddNoteMain = findViewById(R.id.imageAddNoteMain);
+        imageAddNoteMain.setOnClickListener(view -> {
+            startActivityForResult(new Intent(this, CreateNoteActivity.class),
+                    REQUEST_CODE_ADD_NOTE);
+        });
     }
 }
