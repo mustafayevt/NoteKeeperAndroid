97f8e7a840ac7267aaac066ea74b8577bee2358a
mustafayevt <mustafayevtural46@gmail.com>
2021-02-06 15:18:23 +0400

    Android toolbar made hidden in all available activities. Some minor changes

--- app/src/main/java/com/step/notekeeper/CreateNoteActivity.java
+++ app/src/main/java/com/step/notekeeper/CreateNoteActivity.java
@@ -3,6 +3,9 @@ package com.step.notekeeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.Objects;
 
 public class CreateNoteActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class CreateNoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_note);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+
+        ImageView imageBack = findViewById(R.id.imageBack);
+        imageBack.setOnClickListener(view->{
+            onBackPressed();
+        });
     }
 }
